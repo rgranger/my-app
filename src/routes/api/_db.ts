b606fde3e74ab6ba5b179d6f447d1d19ece15d92
Robin Granger <robin.granger@analogway.com>
2022-05-03 11:56:29 +0200

    ch(types) : ajoute des types

--- src/routes/api/_db.ts
+++ src/routes/api/_db.ts
@@ -4,7 +4,7 @@ import bcrypt from 'bcrypt';
 
 let sessions = [];
 
-export const createSession = (username) => {
+export const createSession = (username: string) => {
 	const session = {
 		id: uuidv4(),
 		username
@@ -13,20 +13,20 @@ export const createSession = (username) => {
 	return Promise.resolve(session);
 };
 
-export const getSession = (id) => {
+export const getSession = (id: string) => {
 	const session = sessions.find((session) => session.id === id);
 	if (!session) return Promise.resolve(null);
 	return Promise.resolve(session);
 };
 
-export const removeSession = (id) => {
+export const removeSession = (id: string) => {
 	const session = sessions.find((session) => session.id === id);
 	if (!session) return Promise.reject(new Error('Session not found'));
 	sessions = sessions.filter((session) => session.id !== id);
 	return Promise.resolve(session);
 };
 
-export const checkCredentials = async (username, password) => {
+export const checkCredentials = async (username: string, password: string) => {
 	try {
 		const stmt = db.prepare(`SELECT password FROM users WHERE username = ?;`);
 		const result = stmt.get(username);
@@ -41,7 +41,15 @@ export const checkCredentials = async (username, password) => {
 	}
 };
 
-export const updateCredentials = async ({ username, currentPassword, newPassword }) => {
+export const updateCredentials = async ({
+	username,
+	currentPassword,
+	newPassword
+}: {
+	username: string;
+	currentPassword: string;
+	newPassword: string;
+}) => {
 	try {
 		const currentUser = db.prepare(`select * from users;`).get();
 
