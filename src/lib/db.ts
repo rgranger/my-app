6a34d1c9154be70dc7606507b4fe5c0dbd7d27bf
RGR <rg.robingranger@gmail.com>
2022-05-08 22:32:38 +0200

    cr(env) : utilise DATABASE_URL

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,11 +2,7 @@ import pg from 'pg';
 const { Pool } = pg
 
 export const db = new Pool({
-	user: process.env.PGUSER,
-	host: process.env.PGHOST,
-	database: process.env.PGDATABASE,
-	password: process.env.PGPASSWORD,
-	port: process.env.PGPORT
+	connectionString : process.env.DATABASE_URL,
 });
 
 db.query(`
