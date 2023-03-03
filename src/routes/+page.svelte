d0f2d7484ec9c778a49229ae2605ee3a4a3ce756
Robin Granger <robin.granger@analogway.com>
2023-03-03 14:37:26 +0100

    feat(cv): style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,13 +9,21 @@
 <!-- voir pour idée https://templateflip.com/demo/templates/right-resume/ -->
 
 <div class="relative mx-32 my-16 shadow-lg rounded-xl">
-	<img class="absolute left-8 top-14 border-8 grayscale" src="https://demo.templateflip.com/right-resume/images/avatar.jpg" alt="Robin Granger" width="200" height="200" />
+	<img
+		class="absolute left-8 top-14 border-8 grayscale"
+		src="https://demo.templateflip.com/right-resume/images/avatar.jpg"
+		alt="Robin Granger"
+		width="200"
+		height="200"
+	/>
 	<section class="rounded-t-xl bg-sky-600">
 		<div class="ml-72 w-fit pb-8 pt-14">
-			<h1 class="text-white font-bold">Robin Granger</h1>
+			<div class="text-white font-bold text-4xl mb-4">Robin Granger</div>
 			<p class="text-white mb-4 ">Senior Software Engineer</p>
 			<button class="bg-slate-200 hover:bg-slate-300 py-2 px-4 rounded-full">Download CV</button>
-			<button class="bg-teal-500 hover:bg-teal-600 text-white py-2 px-4 rounded-full ml-1">Hire Me</button>
+			<button class="bg-teal-500 hover:bg-teal-600 text-white py-2 px-4 rounded-full ml-1"
+				>Hire Me</button
+			>
 		</div>
 	</section>
 	<section class="bg-white p-8 pt-12 border-b-2 border-slate-100 flex flex-row gap-8">
@@ -23,16 +31,17 @@
 			<h1>About Me</h1>
 			<p class="">
 				Bonjour ! Je m'appelle Robin Granger et je suis un passioné de développement logiciel.
-				J'affectionne particulièrement l'IA et les dernières technologies, telles que Deno, Sveltekit et Rust.
+				J'affectionne particulièrement l'IA et les dernières technologies, telles que Deno,
+				Sveltekit et Rust.
 			</p>
 		</div>
-		<div class="flex-1 grid gap-6 grid-rows-3 grid-flow-col p-2 h-28">
-				<div>Age</div>
-				<div>Email</div>
-				<div>Adresse</div>
-				<div>31</div>
-				<div>robin.granger.pro@gmail.com</div>
-				<div>Antony</div>
+		<div class="grid gap-6 grid-rows-3 grid-flow-col p-2 h-28 w-80">
+			<div>Age</div>
+			<div>Email</div>
+			<div>Adresse</div>
+			<div>31</div>
+			<div>robin.granger.pro@gmail.com</div>
+			<div>Antony</div>
 		</div>
 	</section>
 	<section class="bg-white p-8 border-b-2 border-slate-100">
@@ -79,7 +88,7 @@
 
 <style>
 	h1 {
-		font-size: x-large;
+		font-size: xx-large;
 		font-weight: bold;
 		text-align: left;
 		margin-bottom: 1rem;
